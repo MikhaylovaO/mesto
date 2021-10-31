5afba25af2cda7069a2b39a988ffd60afbb4f8d0
MikhaylovaO <88785634+MikhaylovaO@users.noreply.github.com>
2021-10-31 18:27:28 +0300

    third

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const popup = document.querySelector(".popup");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const formElement = document.querySelector(".popup__form"); 
-const submitButton = document.querySelector(".popup__submit-button");
-const nameInput = document.querySelector(".popup__input_name");
-const jobInput = document.querySelector(".popup__input_description");
+const nameInput = document.querySelector(".popup__input_type__name");
+const jobInput = document.querySelector(".popup__input_type_description");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__profile-description");
 
@@ -35,6 +34,4 @@ editButton.addEventListener("click", showPopup);
 
 closeButton.addEventListener("click", closePopup);
 
-//если я удаляю const submitButton и её eventlistener, теряется привязка к кнопке сохранения. Додумалась только до такого варианта, если надо оставить один из слушателей.
-
-submitButton.addEventListener("click", formSubmitHandler); 
+formElement.addEventListener("submit", formSubmitHandler); 
