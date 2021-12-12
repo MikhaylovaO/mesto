dd456ae63aedacf5fcff8963982da7fd945473cb
MikhaylovaO <88785634+MikhaylovaO@users.noreply.github.com>
2021-12-12 20:59:45 +0300

    5.3

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const formPicElement = document.querySelector('.popup__form_type_create');
 const popup = document.querySelectorAll('.popup');
 const closeButton = document.querySelectorAll('.popup__close-button');
 const createForm = document.querySelector('.popup__form_type_create');
+const profileForm = document.querySelector('.popup__form_type_profile');
 const cardsContainer = document.querySelector('.elements');
 
 //Постоянные карточки
@@ -60,17 +61,45 @@ function closePopup(popup) {
 //Попап профиль
 
 function showProfilePopup() {
+    profileForm.reset();
+    const profileErrorMessage = profilePopup.querySelectorAll('.popup__input-error');
+    const profileSubmitButton = profilePopup.querySelector('.popup__button');
+
+    makeButtonValid(profileSubmitButton);
+
+    profileErrorMessage.forEach((el) => {
+      deleteErrorMessage(el);
+    });
+    
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
 
     openPopup(profilePopup);
 };
 
+function deleteErrorMessage(el) {
+  el.textContent = '';
+  el.classList.remove('.popup__input-error');
+}
+
+function makeButtonValid(el) {
+  el.classList.remove('.popup__button_disabled');
+  el.disabled = false;
+}
+
 //Попап добавления
 
 function showAddPopup() {
+  const addSubmitButton = addPopup.querySelector('.popup__button');
+
   openPopup(addPopup);
+  makeButtonInvalid(addSubmitButton);
 };
+
+function makeButtonInvalid(el) {
+  el.classList.add('.popup__button_disabled');
+  el.disabled = true;
+}
 
 //Сохранение профиля
 
@@ -158,8 +187,107 @@ formPicElement.addEventListener("submit", function(evt) {
   createForm.reset(); 
 });
 
+// Валидация форм
+
+const showError = (form, input, errorMessageText, errorMessageClass, inputErrorClass) => {
+  const errorMessage = form.querySelector(`#${input.id}-error`);
+  errorMessage.textContent = errorMessageText;
+  errorMessage.classList.add(errorMessageClass);
+  input.classList.add(inputErrorClass);
+}
+
+const hideError = (form, input, errorMessageClass, inputErrorClass) => {
+  const errorMessage = form.querySelector(`#${input.id}-error`);
+  errorMessage.textContent = '';
+  errorMessage.classList.remove(errorMessageClass);
+  input.classList.remove(inputErrorClass);
+}
+
+const hasInvalidInput = (inputs) => {
+  return Array.from(inputs).some((el) => !el.validity.valid);
+}
+
+const toggleButtonError = (inputs, button, inactiveButtonClass) => {
+  if (hasInvalidInput(inputs)) {
+    button.classList.add(inactiveButtonClass);
+    button.disabled = true;
+  } else {
+    button.classList.remove(inactiveButtonClass);
+    button.disabled = false;
+  }
+}
+
+const checkIfInputValid = (form, input, {inputErrorClass, errorClass}) => {
+  if (!input.validity.valid) {
+    showError(form, input, input.validationMessage, errorClass, inputErrorClass);
+  } else {
+    hideError(form, input, inputErrorClass, errorClass);
+  }
+}
+
+const setInputListeners = (form, {inputSelector, submitButtonSelector, inactiveButtonClass, ...rest}) => {
+  const inputs = form.querySelectorAll(inputSelector);
+  const submitButton = form.querySelector(submitButtonSelector);
+
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      checkIfInputValid(form, input, rest);
+      toggleButtonError(inputs, submitButton, inactiveButtonClass);
+    });
+  });
+}
+
+const enableValidation = ({formSelector, ...rest}) => {
+  const forms = document.querySelectorAll(formSelector);
+
+  forms.forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+    });
+
+    setInputListeners(form, rest);
+  })
+}
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error'
+});
+
+// Закрытие на overlay
+
+const popupContainer = document.querySelectorAll('.popup__container');
+
+popupContainer.forEach((element) => {
+document.addEventListener('click', function(evt) {
+  if (evt.target === element) {
+    popup.forEach((element) => {
+      closePopup(element);
+    })
+  }
+})
+})
+
+// Закрытие на Esc
+
+function closeOnEsc(evt) {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    popup.forEach((element) => {
+    closePopup(element);
+    })  
+  }
+}
+
+// Слушатели
+
 editButton.addEventListener("click", showProfilePopup);
 
 formElement.addEventListener("submit", profileFormSubmitHandler); 
 
 addButton.addEventListener("click", showAddPopup);
+
+document.addEventListener("keydown", closeOnEsc);
